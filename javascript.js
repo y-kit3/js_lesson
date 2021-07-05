45074234cfcf7847322c6aa6484d461f34c030e2
y-kit3 <86399381+y-kit3@users.noreply.github.com>
2021-07-05 11:38:34 +0900

    Update javascript.js

---
+++ javascript.js
@@ -0,0 +1,29 @@
+(function() {
+    const target     = document.getElementById('header'),
+          height     = 56;
+    let offset       = 0,
+        lastPosition = 0,
+        ticking      = false;
+        
+    function onScroll() {
+      if (lastPosition > height) {
+        if (lastPosition > offset) {
+          target.classList.add('head-animation');
+        } else {
+          target.classList.remove('head-animation');
+        }
+        offset = lastPosition;
+      }
+    }
+    
+    window.addEventListener('scroll', function(e) {
+      lastPosition = window.scrollY;
+      if (!ticking) {
+        window.requestAnimationFrame(function() {
+          onScroll(lastPosition);
+          ticking = false;
+        });
+        ticking = true;
+      }
+    });
+  })();
