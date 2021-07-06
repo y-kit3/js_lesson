40209a213398e4f2102ac1ef26e225ce9d4636f0
y-kit3 <86399381+y-kit3@users.noreply.github.com>
2021-07-06 14:21:34 +0900

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,10 +1,19 @@
 (function() {
+    /*--変数・定数を定義--*/
     const target     = document.getElementById('header'),
           height     = 56;
     let offset       = 0,
         lastPosition = 0,
         ticking      = false;
-        
+    
+    /*--関数 onScroll() を定義
+    lastPosition が height（56）　より大きい場合以下の処理
+
+      lastPosition が　offset（0）より大きい(正の値の時？)
+      　真：<header> にhead-animation　を追加　、offset に lastPositionの値を代入
+      　偽：<header>　からhead-animationを削除　、offset に lastPositionの値を代入
+    --*/
+
     function onScroll() {
       if (lastPosition > height) {
         if (lastPosition > offset) {
@@ -16,6 +25,22 @@
       }
     }
     
+    /*--イベント処理を実行
+    画面上でスクロールされたとき
+
+    （lastPosition は垂直方向のスクロール量）
+
+　　　ticking が　falseではない時、以下の処理
+　　　
+　　　関数　onScroll　を実行　
+　　　　lastPosition　を引数にする
+　　　　ticking = false　にする
+
+　　　ticking = true　にする
+
+    --*/
+　　
+
     window.addEventListener('scroll', function(e) {
       lastPosition = window.scrollY;
       if (!ticking) {
